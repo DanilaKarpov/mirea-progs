d258f300f6077c0c04409a2cd3c5dab7b82f08a2
Danila Karpov <75839359+DanilaKarpov@users.noreply.github.com>
2020-12-28 12:29:19 +0300

    Update Task_3.jl

--- Task_3.jl
+++ Task_3.jl
@@ -1,7 +1,7 @@
 function mark_all(r::Robot)
     nun_vert = get_num_steps_movements!(r,Sud)
     nun_hor = get_num_steps_movements!(r,West)
-    #УТВ: Робот - в юго-западном углу
+    #Робот - в юго-западном углу
 
     side = Ost
     mark_row!(r,side)
@@ -9,12 +9,44 @@ function mark_all(r::Robot)
         side=inverse(side)
         mark_row!(r,side)
     end
-    #УТВ: Робот - у северной границы, в одном из углов
+    #Робот - у северной границы, в одном из углов
 
     movements!(r,Sud)
     movements!(r,West)
-    #УТВ: Робот - в юго-западном углу
+    #Робот - в юго-западном углу
 
     movemens!(r,Ost,num_hor)
     movemens!(r,Nord,num_vert)
+
+function moves!(r::Robot,side::HorizonSide)
+    num_steps=0
+    while isborder(r,side)==false
+        move!(r,side)
+        num_steps+=1
+    end
+    return num_steps
 end
+    
+function moves!(r::Robot,side::HorizonSide,num_steps::Int)
+    for _ in 1:num_steps
+        move!(r,side)
+    end
+end
+    
+function putmarkers!(r::Robot,side::HorizonSide)
+    while isborder(r,side)==false
+        putmarker!(r) 
+        move!(r,side)
+    end
+    putmarker!(r)
+    move!(r,Nord)
+end
+    
+function putmarkers_last!(r::Robot,side::HorizonSide)
+    while isborder(r,side)==false
+        putmarker!(r) 
+        move!(r,side)
+    end
+    putmarker!(r)
+end
+inverse(side::HorizonSide)=HorizonSide(mod(Int(side)+2,4))
