9d99ea349bc6936fae528e5d7067fc08509bdd76
Danila Karpov <75839359+DanilaKarpov@users.noreply.github.com>
2020-12-28 12:51:29 +0300

    Update Task_7.jl

--- Task_7.jl
+++ Task_7.jl
@@ -1,8 +1,8 @@
 using .ShessMark, .StatBack
 
 function mark_chess(robot)
-    init(isodd(sum(move_to_start!((Sud,West)))) # isodd - возвращает true, если аргумент нечетный
-    #УТВ: Робот - в юго-западном углу и глобальная переменная модуля инициализирована
+    init(isodd(sum(move_to_start!((Sud,West))))
+    #Робот - в юго-западном углу и глобальная переменная модуля инициализирована
 
     side = Ost
     mark_chess(robot,side)
@@ -11,17 +11,17 @@ function mark_chess(robot)
         side = inverse(side)
         mark_chess(robot,side)
     end
-    #УТВ: Робот - у северной границы поля И маркеры расставлены в шахматном порядке
+    #Робот - у северной границы поля И маркеры расставлены в шахматном порядке
 
     for side in (West,Sud) movements!(robot,side) end
     #УТВ: Робот - в юго-западном углу
 
     move_to_back(robot)
-    #УТВ: Робот - в исходном положении
+    #Робот - в исходном положении
 end
 
 function mark_chess(r,side)
-    ShessMark.putmarker!(r) # здесь вызывается не стандартная команда Робота, а одноименная функция из модуля
+    ShessMark.putmarker!(r)
     while isborder(r,side)==false
         move!(r,side)
         ShessMark.putmarker!(r)
